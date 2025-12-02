4e0eaf15b620b4e2d2d0d0b4c6189228df730c3c
Abhishek Raj <122792587+abhii-raj@users.noreply.github.com>
2025-12-02 19:48:59 +0530

    Reorder middleware and database connection

--- CircleUp/server/server.js
+++ CircleUp/server/server.js
@@ -11,15 +11,15 @@ import storyRouter from './routes/storyRoutes.js';
 import messageRouter from './routes/messageRoutes.js';
 
 const app = express();
-
-await connectDB();
-
-app.use(express.json());
 app.use(cors({
   origin: "http://localhost:5173", 
   credentials: true,               
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"]
 }));
+await connectDB();
+
+app.use(express.json());
+
 app.use(clerkMiddleware());
 
 app.get('/', (req, res)=> res.send('Server is running'))
